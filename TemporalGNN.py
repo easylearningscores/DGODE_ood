3a6dd8a13f70912712e97c680fa02f22ec0a2eac
Hao Wu <133833074+easylearningscores@users.noreply.github.com>
2024-05-27 12:09:55 +0800

    Update TemporalGNN.py

--- TemporalGNN.py
+++ TemporalGNN.py
@@ -1,137 +1,70 @@
-from torch import nn
 import torch
-from utils import *
+import torch.nn as nn
+import torch.nn.functional as F
 
-class E_GCL(nn.Module):
-    def __init__(self, input_nf, output_nf, hidden_nf, edges_in_d=0, act_fn=nn.SiLU(), residual=True, attention=False, normalize=False, coords_agg='mean', tanh=False):
-        super(E_GCL, self).__init__()
-        input_edge = input_nf * 2
-        self.residual = residual
-        self.attention = attention
-        self.normalize = normalize
-        self.coords_agg = coords_agg
-        self.tanh = tanh
-        self.epsilon = 1e-8
-        edge_coords_nf = 1
+class TemporalGNNLayer(nn.Module):
+    def __init__(self, in_features, out_features):
+        super(TemporalGNNLayer, self).__init__()
+        self.phi_e = nn.Linear(3 * in_features, out_features)
+    
+    def forward(self, h_i_t, h_i_t_1, N_i_t):
+        N_i_t_agg = torch.mean(N_i_t, dim=2)  
+        concat_features = torch.cat([h_i_t, h_i_t_1, N_i_t_agg], dim=-1)
+        h_i_t_l = F.relu(self.phi_e(concat_features))
+        return h_i_t_l
 
-        self.edge_mlp = nn.Sequential(
-            nn.Linear(input_edge + edge_coords_nf + edges_in_d, hidden_nf),
-            act_fn,
-            nn.Linear(hidden_nf, hidden_nf),
-            act_fn)
+class TemporalGNN(nn.Module):
+    def __init__(self, in_features, out_features, num_layers, num_nodes):
+        super(TemporalGNN, self).__init__()
+        self.layers = nn.ModuleList([TemporalGNNLayer(in_features if i == 0 else out_features, out_features) for i in range(num_layers)])
+        self.num_layers = num_layers
+        self.num_nodes = num_nodes
 
-        self.node_mlp = nn.Sequential(
-            nn.Linear(hidden_nf + input_nf, hidden_nf),
-            act_fn,
-            nn.Linear(hidden_nf, output_nf))
+    def forward(self, node_features):
+        h = node_features
+        batch_size = h.shape[0]
+        for l in range(self.num_layers):
+            h_new = []
+            for i in range(self.num_nodes):
+                h_i_t = h[:, :, i, :]
+                h_i_t_1 = torch.cat([torch.zeros(batch_size, 1, h_i_t.shape[2]).to(h.device), h_i_t[:, :-1, :]], dim=1)
+                N_i_t = h
+                h_i_t_l = self.layers[l](h_i_t, h_i_t_1, N_i_t)
+                h_new.append(h_i_t_l.unsqueeze(2))
+            h = torch.cat(h_new, dim=2)
+        return h
 
-        layer = nn.Linear(hidden_nf, 1, bias=False)
-        torch.nn.init.xavier_uniform_(layer.weight, gain=0.001)
+class NodeFeatureSummarizer(nn.Module):
+    def __init__(self):
+        super(NodeFeatureSummarizer, self).__init__()
 
-        coord_mlp = []
-        coord_mlp.append(nn.Linear(hidden_nf, hidden_nf))
-        coord_mlp.append(act_fn)
-        coord_mlp.append(layer)
-        if self.tanh:
-            coord_mlp.append(nn.Tanh())
-        self.coord_mlp = nn.Sequential(*coord_mlp)
+    def forward(self, temporal_node_representations):
+        spatial_node_features = torch.sum(temporal_node_representations, dim=1)
+        return spatial_node_features
 
-        if self.attention:
-            self.att_mlp = nn.Sequential(
-                nn.Linear(hidden_nf, 1),
-                nn.Sigmoid())
+class TemporalGNNModel(nn.Module):
+    def __init__(self, in_features, out_features, num_layers, num_nodes):
+        super(TemporalGNNModel, self).__init__()
+        self.temporal_gnn = TemporalGNN(in_features, out_features, num_layers, num_nodes)
+        self.summarizer = NodeFeatureSummarizer()
 
-    def edge_model(self, source, target, radial, edge_attr):
-        if edge_attr is None:  # Unused.
-            out = torch.cat([source, target, radial], dim=1)
-        else:
-            out = torch.cat([source, target, radial, edge_attr], dim=1)
-        out = self.edge_mlp(out)
-        if self.attention:
-            att_val = self.att_mlp(out)
-            out = out * att_val
-        return out
-
-    def node_model(self, x, edge_index, edge_attr, node_attr):
-        row, col = edge_index
-        agg = unsorted_segment_sum(edge_attr, row, num_segments=x.size(0))
-        if node_attr is not None:
-            agg = torch.cat([x, agg, node_attr], dim=1)
-        else:
-            agg = torch.cat([x, agg], dim=1)
-        out = self.node_mlp(agg)
-        if self.residual:
-            out = x + out
-        return out, agg
-
-    def coord_model(self, coord, edge_index, coord_diff, edge_feat):
-        row, col = edge_index
-        trans = coord_diff * self.coord_mlp(edge_feat)
-        if self.coords_agg == 'sum':
-            agg = unsorted_segment_sum(trans, row, num_segments=coord.size(0))
-        elif self.coords_agg == 'mean':
-            agg = unsorted_segment_mean(trans, row, num_segments=coord.size(0))
-        else:
-            raise Exception('Wrong coords_agg parameter' % self.coords_agg)
-        coord = coord + agg
-        return coord
-
-    def coord2radial(self, edge_index, coord):
-        row, col = edge_index
-        coord_diff = coord[row] - coord[col]
-        radial = torch.sum(coord_diff**2, 1).unsqueeze(1)
-
-        if self.normalize:
-            norm = torch.sqrt(radial).detach() + self.epsilon
-            coord_diff = coord_diff / norm
-
-        return radial, coord_diff
-
-    def forward(self, h, edge_index, coord, edge_attr=None, node_attr=None):
-        row, col = edge_index
-        radial, coord_diff = self.coord2radial(edge_index, coord)
-
-        edge_feat = self.edge_model(h[row], h[col], radial, edge_attr)
-        coord = self.coord_model(coord, edge_index, coord_diff, edge_feat)
-        h, agg = self.node_model(h, edge_index, edge_feat, node_attr)
-
-        return h, coord, edge_attr
+    def forward(self, node_features):
+        temporal_node_representations = self.temporal_gnn(node_features)
+        spatial_node_features = self.summarizer(temporal_node_representations)
+        return spatial_node_features
 
 
-class Equivariant_Graph_Operator(nn.Module):
-    def __init__(self, in_node_nf, hidden_nf, out_node_nf, in_edge_nf=0, device='cpu', act_fn=nn.SiLU(), n_layers=4, residual=True, attention=False, normalize=False, tanh=False):
-        super(Equivariant_Graph_Operator, self).__init__()
-        self.hidden_nf = hidden_nf
-        self.device = device
-        self.n_layers = n_layers
-        self.embedding_in = nn.Linear(in_node_nf, self.hidden_nf)
-        self.embedding_out = nn.Linear(self.hidden_nf, out_node_nf)
-        for i in range(0, n_layers):
-            self.add_module("gcl_%d" % i, E_GCL(self.hidden_nf, self.hidden_nf, self.hidden_nf, edges_in_d=in_edge_nf,
-                                                act_fn=act_fn, residual=residual, attention=attention,
-                                                normalize=normalize, tanh=tanh))
-        self.to(self.device)
+in_features = 1
+out_features = 1
+num_layers = 3
+num_nodes = 1024
+T_obs = 5
+batch_size = 2
 
-    def forward(self, h, x, edges, edge_attr):
-        h = self.embedding_in(h)
-        for i in range(0, self.n_layers):
-            h, x, _ = self._modules["gcl_%d" % i](h, edges, x, edge_attr=edge_attr)
-        h = self.embedding_out(h)
-        return h, x
+model = TemporalGNNModel(in_features, out_features, num_layers, num_nodes)
 
-if __name__ == "__main__":
-    batch_size = 5
-    n_nodes = 1024 # 64x64
-    n_feat = 1
-    x_dim = 2
+# (batch_size, T_obs, num_nodes, in_features)
+node_features = torch.randn(batch_size, T_obs, num_nodes, in_features)
 
-    h = torch.rand(batch_size *  n_nodes, n_feat)
-    x = torch.rand(batch_size * n_nodes, x_dim)
-    edges, edge_attr = get_edges_batch(n_nodes, batch_size)
-    print("input shape:", h.shape, x.shape)
-
-    egnn = Equivariant_Graph_Operator(in_node_nf=n_feat, hidden_nf=32, out_node_nf=1, in_edge_nf=1)
-
-
-    h, x = egnn(h, x, edges, edge_attr)
-    print("output shape:", h.shape)
+spatial_node_features = model(node_features)
+print(spatial_node_features.shape)  # (batch_size, num_nodes, out_features)
